37f6819663a331fc9950f7d91760f4432728c64d
Sami Farhat <samikfarhat@gmail.com>
2018-07-03 16:38:32 +0100

    many additions to test - but they will be refactored to use async / await

--- test/test-main.js
+++ test/test-main.js
@@ -3,47 +3,85 @@
   var EthBnB = artifacts.require("EthBnB");
 
   contract('EthBnB', function(accounts) {
-    it('should create a listing correctly', function() {
+
+    /**
+     * checks that hasAccount() returns false when no account has been created
+     */
+    it('Account: hasAcccount returns false when no account has been created', function() {
+      var bnb; 
+      return EthBnB.deployed().then(function(instance) {
+        bnb = instance
+        var accountExists0 = bnb.hasAccount.call({from: accounts[0]}).then(function(account0Exists) {
+          assert.isFalse(account0Exists, "Account 0 exists for some reason, shouldn't be the case.");
+        });
+        var accountExists1 = bnb.hasAccount.call({from: accounts[1]}).then(function(account1Exists) {
+          assert.isFalse(account1Exists, "Account 1 exists for some reason, shouldn't be the case.");
+        });
+      });
+    });
+
+    /**
+     * checks that we can create accounts correctly and that the returned name is correct
+     */
+    it('Account: createAccount correct', function() {
+      var bnb; 
+
+      return EthBnB.deployed().then(function(instance) {
+        bnb = instance;
+
+        var _shortName = "sami";
+        bnb.createAccount(_shortName, {from: accounts[0]});
+        var accountExists = bnb.hasAccount.call( {from: accounts[0]} );
+        assert(accountExists, "createAccount doesn't seem to have created an account");
+        bnb.getName.call(function(actualName) {
+          assert(actualName === _shortName, "The account shortName does not match what we expect.");
+        });
+      });
+    });
+
+
+    it('Listing: createListing correct', function() {
       var bnb;
 
-      return EthBnB.deployed()
-      /*
-       * returns the id and fields of the listing that needs to be created.
-       * {
-       * 'id': returned_id, 
-       * 'price': _price,
-       * 'description': _description
-       * }
-       */
-        .then(function(instance) {
-          // Create Listing
-          // -------------
-          console.log(instance);
-          bnb = instance;
+      return EthBnB.deployed().then(function(instance) {
+        bnb = instance;
 
+        // create account
+        var _shortName = "Sami";
+        bnb.createAccount.call(_shortName, {from: accounts[0]}).then(function() {
           // create a listing
           var _location = "London";
           var _price = 5000;
           var _shortName = "Sami's awesome place";
           var _description = "You must be really awesome and Sami-approved to get the room there.\n" + 
             "Also, you must invite him to all of the parties.";
-          var id = bnb.createListing(_location, _price, _shortName, _description, {from : accounts[0]});
-          return {
-            'id' : id, 
-            'description' : _description, 
-            'location' : _location,
-            'price' :  _price, 
-            'shortName' : shortName,
-          };
-        })
-        .then(function(listingData) {
-          // Check its presence
-          // ------------------
-          var id = listingData.id;
-          assert(id > 0, "The returned Id was zero or negative.");
-
+          bnb.createListing.call(_location, _price, _shortName, _description, {from : accounts[0]})
+            .then(function(listingId) {
+              assert(listingId > 0, "The returned Id was zero or negative.");
+            });
         });
+      })
     });
-  });
 
+    /**
+     * create listing without an account
+     */
+    it('Listing: createListing fails when user has no \'Account\'', function() {
+      var bnb;
+      EthBnB.deployed().then(function(instance) {
+        bnb = instance;
+        // create a listing
+        var _location = "London";
+        var _price = 5000;
+        var _shortName = "Sami's awesome place";
+        var _description = "You must be really awesome and Sami-approved to get the room there.\n" + 
+          "Also, you must invite him to all of the parties.";
+        bnb.createListing.call(_location, _price, _shortName, _description, {from : accounts[0]})
+          .then(function(listingId) {
+            assert(listingId < 1, "The returned Id was zero or negative.");
+          });
+      });
+    });
+
+  });
 })();
