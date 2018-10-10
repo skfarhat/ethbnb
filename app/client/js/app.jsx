bae201b64fc5e35372d043e6e3255a8fcb5d0c5f
Sami Farhat <samikfarhat@gmail.com>
2018-10-10 14:57:58 +0100

    Formatting changes: no logic changes / additions

--- app/client/js/app.jsx
+++ app/client/js/app.jsx
@@ -1,70 +1,73 @@
 var Web3 = require('web3')
-const PROVIDER_STR = "http://localhost:8545"
+const PROVIDER_STR = 'http://localhost:8545'
 
 class App extends React.Component {
-    constructor(props) {
-        console.log("App: constructor")
-        super(props)
-        this.state = {
-            num_clients: 3, 
-        } 
-        this.setupState()
+  constructor(props) {
+    console.log('App: constructor')
+    super(props)
+    this.state = {
+      num_clients: 3,
     }
-    async setupState() {
-        console.log("App: setupState BEGIN")
-        await this.setupEth()
-        // This will be called after we're done setting up all eth, so we call setState
-        // and trigger a redrawing of children 
-        this.setState(this.state)
-        console.log("App: setupState DONE")
-    }
-    async setupEth() {
-        console.log("App: setupEth")
-        var provider
-        if (typeof this.web3 !== 'undefined') {
-            provider = this.web3.currentProvider;
-        } else {
-            // If no injected web3 instance is detected, fall back to Ganache
-            provider = new Web3.providers.HttpProvider(PROVIDER_STR);
-        }
-        
-        // Create an instance of web3 using the HTTP provider.
-        // NOTE in mist web3 is already available, so check first if it's available before instantiating
-        let web3 = new Web3(provider)
+    this.setupState()
+  }
 
-        // Load ABI into contract
-        let abiArray = window.abiArray // get it from somewhere
-        let MyContract = TruffleContract(abiArray)
-        MyContract.setProvider(provider)
-        let contractInstance = await MyContract.deployed()
-        
-        // Set properties on `this.state.eth`
-        this.state.web3 = web3
-        this.state.accounts = web3.eth.accounts
-        this.state.MyContract = MyContract
-        this.state.contractInstance = contractInstance
+  async setupState() {
+    console.log('App: setupState BEGIN')
+    await this.setupEth()
+    // This will be called after we're done setting up all eth, so we call setState
+    // and trigger a redrawing of children
+    this.setState(this.state)
+    console.log('App: setupState DONE')
+  }
+
+  async setupEth() {
+    let provider
+    console.log('App: setupEth')
+    if (typeof this.web3 !== 'undefined') {
+      provider = this.web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      provider = new Web3.providers.HttpProvider(PROVIDER_STR);
     }
-    render() {
-        console.log("App: render")
-        return (
-            <div>
-                <div id="div-top" className="row">
-                    <Common />
-                </div>
-                <div id="div-content" className="row">
-                    <div id="clients-container" className="col-8"> 
-                        <ClientsManager ctxt={this.state}/> 
-                    </div>
-                    <div id="api-container" className="col-4">
-                        <APICaller ctxt={this.state}/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+
+    // Create an instance of web3 using the HTTP provider.
+    // NOTE in mist web3 is already available, so check first if it's available before instantiating
+    const web3 = new Web3(provider)
+
+    // Load ABI into contract
+    const abiArray = window.abiArray // get it from somewhere
+    const MyContract = TruffleContract(abiArray)
+    MyContract.setProvider(provider)
+    const contractInstance = await MyContract.deployed()
+
+    // Set properties on `this.state.eth`
+    this.state.web3 = web3
+    this.state.accounts = web3.eth.accounts
+    this.state.MyContract = MyContract
+    this.state.contractInstance = contractInstance
+  }
+
+  render() {
+    console.log('App: render')
+    return (
+      <div>
+        <div id="div-top" className= "row">
+          <Common />
+        </div>
+        <div id= "div-content" className="row" >
+          <div id="clients-container" className="col-8" >
+            <ClientsManager ctxt={this.state} />
+          </div>
+        <div id="api-container" className="col-4" >
+          <APICaller ctxt={this.state} />
+        </div>
+      </div>
+    </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
--- app/client/js/client.jsx
+++ app/client/js/client.jsx
@@ -1,43 +1,47 @@
 function Client(props) {
-    let id = props.clientId
-    let account_id = props.clientAddress
-    return (            
-        <div className="col client-div" data-client-id="{id}">
-            <h2> Client {id} </h2>
-            <div>
-                <em> Account ID </em> <span id="account_id"> {account_id} </span>
-            </div>
-        </div>
-    )
+  const id = props.clientId
+  const accountId = props.clientAddress
+  return (
+    <div className="col client-div" data-client-id="{id}">
+      <h2> Client {id} </h2>
+      <div>
+        <em> Account ID </em>
+        <span id="accountId"> {accountId} </span>
+      </div>
+    </div>
+  )
 }
+
 class ClientsManager extends React.Component {
-    constructor(props) {
-        super(props)
-        this.eth = props.ctxt
-    }
-    getClients() {
-        console.log("ClientsManager: getClients()")
-        let all_clients = []; 
-        for (var i = 0; i < this.eth.num_clients; i++) {
-            if (this.eth.accounts) {
-                all_clients.push(
-                    <Client 
-                    key={i} 
-                    clientId={i} 
-                    clientAddress={this.eth.accounts[i]} 
-                    />
-                    )
-            }
-        }
-        return all_clients
-    }
-    render() {
-        console.log("ClientsManager: render")
-        return (
-            <div id="div-clients">
-                <h2> Clients </h2>
-                { this.getClients() }
-            </div>
+  constructor(props) {
+    super(props)
+    this.eth = props.ctxt
+  }
+
+  getClients() {
+    console.log('ClientsManager: getClients()')
+    let all_clients = [];
+    for (var i = 0; i < this.eth.num_clients; i++) {
+      if (this.eth.accounts) {
+        all_clients.push(
+          <Client
+          key={i}
+          clientId={i}
+          clientAddress={this.eth.accounts[i]}
+          />
         )
+      }
     }
+    return all_clients
+  }
+
+  render() {
+    console.log('ClientsManager: render')
+    return (
+      <div id="div-clients">
+        <h2> Clients </h2>
+          { this.getClients() }
+      </div>
+    )
+  }
 }
