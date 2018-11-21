9a716b43fabe4770ff3627edcd270dbb97452bc8
Sami Farhat <samikfarhat@gmail.com>
2018-11-21 11:17:14 +0000

    WIP: Improve IPFS implementation

--- app/client/src/components/EthManager.jsx
+++ app/client/src/components/EthManager.jsx
@@ -4,7 +4,9 @@ import { connect } from "react-redux"
 import TruffleContract from 'truffle-contract'
 import EthEventListener from './EthEventListener'
 import ipfs from './IPFS'
+import buffer from 'buffer'
 import "../loadAbi.js"
+
 import { refreshEth, createAccount, addMessage, createListing } from "../actions/"
 
 var Web3 = require("web3")
@@ -29,49 +31,12 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-// public method for encoding an Uint8Array to base64
-// Shamelessly stolen off some stackoverflow response
-function encode(input) {
-  var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-  var output = "";
-  var chr1,
-    chr2,
-    chr3,
-    enc1,
-    enc2,
-    enc3,
-    enc4;
-  var i = 0;
-
-  while (i < input.length) {
-    chr1 = input[i++];
-    chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
-    chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
-
-    enc1 = chr1 >> 2;
-    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-    enc4 = chr3 & 63;
-
-    if (isNaN(chr2)) {
-      enc3 = enc4 = 64;
-    } else if (isNaN(chr3)) {
-      enc4 = 64;
-    }
-    output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-    keyStr.charAt(enc3) + keyStr.charAt(enc4);
-  }
-  return output;
-}
-
 class EthManager_ extends Component {
 
   constructor(props) {
     log.debug("EthManager_")
     super(props)
 
-    this.uploadPicture = this.uploadPicture.bind(this)
-    this.getImage = this.getImage.bind(this)
     // The event listener will be instantiated when this.setupEth() completes
     // and registerEvents() will be invoked on the resulting object
     this.eventListener = null
@@ -116,64 +81,33 @@ class EthManager_ extends Component {
   }
 
   async uploadPicture(evt) {
-    const self = this
-    console.log('uploadPicture', evt.target.files)
-    const files = Array.from(evt.target.files)
-    log.debug('uploadPicture')
-    const data = new FormData()
-    const file = files[0]
-    data.append('file', file, file.name)
-    console.log('the data is ', data)
-
-    var reader = new FileReader();
-    reader.onload = async function(e) {
-      // var buff = reader.result;
-      // console.log("read the file as ", buff)
-      //file is converted to a buffer for upload to IPFS
-      const buff = await Buffer.from(reader.result);
-      await ipfs.add(buff, (err, ipfsHash) => {
-        if (err) {
-          console.log("we hit an error", err)
-        } else {
-          console.log("the ipfs hash is ", err, ipfsHash)
-        }
-        self.ipfs = {
-          ipfsInstance: ipfsHash,
-        }
-      })
+    const reader = new FileReader();
+    reader.onloadend = function() {
+      const buf = buffer.Buffer(reader.result) // Convert data into buffer
+      try {
+        ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+          if (err || !result) {
+            console.error(err)
+          } else {
+            const hash = result[0].hash
+            ipfs.files.cat(hash, (err, data) => {
+              const blob = new Blob([data], {
+                type: "image/png"
+              })
+              const url = window.URL.createObjectURL(blob)
+              const img = document.getElementById('ItemPreview')
+              img.src = url
+            })
+          }
+        })
+      } catch (err) {
+        console.log('Exception thrown with error ', err)
+      }
     }
 
-
-
-    reader.readAsBinaryString(file);
-  // // TODO: read the file image we want to post 
-  // //       and have it be in a buffer 
-  // const photoBuffer = {}
-  // await ipfs.add(photoBuffer, (err, ipfsHash) => {
-  //   console.log('ipfs.add', err, ipfsHash)
-  // })
-  }
-
-  getImage(evt) {
-
-    // console.log('getImage', this)
-    // const ipfsInstance = this.ipfs.ipfsInstance
-    // console.log('ipfsinstance is', ipfsInstance)
-    // const {hash} = ipfsInstance[0]
-    console.log("'the hash to be used is ", hash)
-    const hash = "QmcD8XJcikRvHjJXuMuUNLZ52fnRUP6SzqMbsSDAZXG7w1"
-    ipfs.cat(hash, function(err, file) {
-      if (err) {
-        throw err
-      }
-
-      // converting the received data into an "image"
-      var bytes = new Uint8Array(file);
-
-      var image = document.getElementById('ItemPreview'); // IdOfImage is the id attribute of the img tag in your html page
-      image.src = "data:image/png;base64," + encode(bytes);
-
-    })
+    // Get the uploaded file image
+    const photo = document.getElementById("image-file");
+    reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
   }
 
   getClientObjFromAddress(addr) {
@@ -186,20 +120,20 @@ class EthManager_ extends Component {
 
   render() {
     return (
-      <div className="row" hidden={false}> 
+      <div className="row" hidden={false}>
       <input
+      id="image-file"
       key="button"
       className="btn btn-default"
       type="file"
       name="Add file to IPFS"
-      onChange={(evt) => this.uploadPicture(evt)}
       />
-      <div> 
-      <img id="ItemPreview" src="" />
+      <div>
+      <img width="200px" id="ItemPreview" src="" />
       <button
       className="btn btn-default"
       onClick={(evt) => {
-        this.getImage()
+        this.uploadPicture(evt)
       }}
       >
       Get image
