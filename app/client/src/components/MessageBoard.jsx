c5202ebc9c18b3893f5ebbd76e96a09482ddc833
Sami Farhat <samikfarhat@gmail.com>
2018-11-22 10:07:00 +0000

    Minor: More code cleanup

--- app/client/src/components/MessageBoard.jsx
+++ app/client/src/components/MessageBoard.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react'
-import { connect } from "react-redux";
-import log from "../logger"
+import { connect } from 'react-redux'
+import log from '../logger'
 
 const mapStateToProps = (state) => {
-    return {
-        messages: state.messages
-    }
+  return {
+    messages: state.messages
+  }
 }
 
 class Message extends Component {
-    render() {
-        return (<p> {this.props.msg.text} </p>)
-    }
+  render() {
+    return (<p> {this.props.msg.text} </p>)
+  }
 }
 
 class MessageBoard_ extends Component {
-    render() {
-        log.debug('MessageBoard: render')
-        let messages = []
-        for (var i in this.props.messages) {
-            let msg = this.props.messages[i]
-            messages.push(<Message key={i} msg={msg} />)
-        }
-        return (
-            <div id='message-board' className={this.props.bootstrapWidth}> 
+  render() {
+    log.debug('MessageBoard: render')
+    let messages = []
+    for (var i in this.props.messages) {
+      let msg = this.props.messages[i]
+      messages.push(<Message key={i} msg={msg} />)
+    }
+    return (
+      <div id='message-board' className={this.props.bootstrapWidth}>
                 <h3> MessageBoard </h3>
                 {messages}
             </div>
-            )
-    }
+    )
+  }
 }
 
 const MessageBoard = connect(mapStateToProps, null)(MessageBoard_)
