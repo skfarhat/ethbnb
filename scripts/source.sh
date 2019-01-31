f3e541687fcf8b5044ee77172f8ccd4be85c0dab
Sami Farhat <samikfarhat@gmail.com>
2019-01-31 06:49:53 +0000

    scripts/source.sh: fix $CLIENTDIR was wrong

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 #
 # 
-DIR=$(cd $(dirname "${BASH_SOURCE[0]}" ) && pwd )
-CLIENTDIR="$DIR/app/client"
+SCRIPTS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}" ) && pwd )
+ROOT_DIR=$(cd $SCRIPTS_DIR && cd .. && pwd)
+CLIENT_DIR="$ROOT_DIR/app/client"
 
 lint() {
   d=$(mktemp -d)
-  pushd $CLIENTDIR
+  pushd $CLIENT_DIR
   eslint --config .eslintrc.json --ext .jsx --format html -o $d/eslint-output.html .
   popd
   open $d/eslint-output.html
