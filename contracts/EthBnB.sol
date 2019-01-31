4becd16a47edd64688a41ff8e4e7c1c2d390028a
Sami Farhat <samikfarhat@gmail.com>
2019-01-31 12:32:23 +0000

    EthBnB.sol,test-main: set country in Listing constructor. Adjust tests.
    All tests pass.

--- contracts/EthBnB.sol
+++ contracts/EthBnB.sol
@@ -16,7 +16,7 @@ contract EthBnB {
 
   enum Country {
    AF,AL,DZ,AS,AD,AO,AI,AQ,AG,AR,AM,AW,AU,AT,AZ,BS,BH,BD,BB,BY,BE,BZ,BJ,BM,BT,BO,BA,BW,BV,BR,IO,BN,BG,BF,BI,KH,CM,CA,CV,KY,CF,TD,CL,CN,CX,CC,CO,KM,CG,CD,CK,CR,CI,HR,CU,CY,CZ,DK,DJ,DM,DO,TP,EC,EG,SV,GQ,ER,EE,ET,FK,FO,FJ,FI,FR,GF,PF,TF,GA,GM,GE,DE,GH,GI,GR,GL,GD,GP,GU,GT,GN,GW,GY,HT,HM,VA,HN,HK,HU,IS,IN,ID,IR,IQ,IE,IL,IT,JM,JP,JO,KZ,KE,KI,KP,KR,KV,KW,KG,LA,LV,LB,LS,LR,LY,LI,LT,LU,MO,MK,MG,MW,MY,MV,ML,MT,MH,MQ,MR,MU,YT,MX,FM,MD,MC,MN,MS,ME,MA,MZ,MM,NA,NR,NP,NL,AN,NC,NZ,NI,NE,NG,NU,NF,MP,NO,OM,PK,PW,PS,PA,PG,PY,PE,PH,PN,PL,PT,PR,QA,RE,RO,RU,RW,SH,KN,LC,PM,VC,WS,SM,ST,SA,SN,RS,SC,SL,SG,SK,SI,SB,SO,ZA,GS,ES,LK,SD,SR,SJ,SZ,SE,CH,SY,TW,TJ,TZ,TH,TG,TK,TO,TT,TN,TR,TM,TC,TV,UG,UA,AE,GB,US,UM,UY,UZ,VU,VE,VN,VG,VI,WF,EH,YE,ZM,ZW
- }
+  }
 
   /**
   * @title Represents a single image which is owned by someone.
@@ -56,7 +56,8 @@ contract EthBnB {
     uint price;
 
     string location;
-
+    
+    Country country; 
 
     /**
      * stores all bookings for this Listing with
@@ -207,13 +208,14 @@ contract EthBnB {
    * Creates a new listing for the message sender
    * and returns the Id of the created listing
    */
-   function createListing(string location, uint price, string shortName, string desc) public {
+   function createListing(Country country, string location, uint price) public {
     require(hasAccount(), "Must have an account before creating a listing");
     // Note: enforce a maximum number of listings per user?
 
     listings[nextListingId] = Listing({
       id : nextListingId,
       owner: msg.sender,
+      country: country,
       location: location,
       price: price
       });
--- test/test-main.js
+++ test/test-main.js
@@ -3,6 +3,8 @@
 (function() {
   'use strict'
 
+  const COUNTRIES = {"AF" : 0,"AL" : 1,"DZ" : 2,"AS" : 3,"AD" : 4,"AO" : 5,"AI" : 6,"AQ" : 7,"AG" : 8,"AR" : 9,"AM" : 10,"AW" : 11,"AU" : 12,"AT" : 13,"AZ" : 14,"BS" : 15,"BH" : 16,"BD" : 17,"BB" : 18,"BY" : 19,"BE" : 20,"BZ" : 21,"BJ" : 22,"BM" : 23,"BT" : 24,"BO" : 25,"BA" : 26,"BW" : 27,"BV" : 28,"BR" : 29,"IO" : 30,"BN" : 31,"BG" : 32,"BF" : 33,"BI" : 34,"KH" : 35,"CM" : 36,"CA" : 37,"CV" : 38,"KY" : 39,"CF" : 40,"TD" : 41,"CL" : 42,"CN" : 43,"CX" : 44,"CC" : 45,"CO" : 46,"KM" : 47,"CG" : 48,"CD" : 49,"CK" : 50,"CR" : 51,"CI" : 52,"HR" : 53,"CU" : 54,"CY" : 55,"CZ" : 56,"DK" : 57,"DJ" : 58,"DM" : 59,"DO" : 60,"TP" : 61,"EC" : 62,"EG" : 63,"SV" : 64,"GQ" : 65,"ER" : 66,"EE" : 67,"ET" : 68,"FK" : 69,"FO" : 70,"FJ" : 71,"FI" : 72,"FR" : 73,"GF" : 74,"PF" : 75,"TF" : 76,"GA" : 77,"GM" : 78,"GE" : 79,"DE" : 80,"GH" : 81,"GI" : 82,"GR" : 83,"GL" : 84,"GD" : 85,"GP" : 86,"GU" : 87,"GT" : 88,"GN" : 89,"GW" : 90,"GY" : 91,"HT" : 92,"HM" : 93,"VA" : 94,"HN" : 95,"HK" : 96,"HU" : 97,"IS" : 98,"IN" : 99,"ID" : 100,"IR" : 101,"IQ" : 102,"IE" : 103,"IL" : 104,"IT" : 105,"JM" : 106,"JP" : 107,"JO" : 108,"KZ" : 109,"KE" : 110,"KI" : 111,"KP" : 112,"KR" : 113,"KV" : 114,"KW" : 115,"KG" : 116,"LA" : 117,"LV" : 118,"LB" : 119,"LS" : 120,"LR" : 121,"LY" : 122,"LI" : 123,"LT" : 124,"LU" : 125,"MO" : 126,"MK" : 127,"MG" : 128,"MW" : 129,"MY" : 130,"MV" : 131,"ML" : 132,"MT" : 133,"MH" : 134,"MQ" : 135,"MR" : 136,"MU" : 137,"YT" : 138,"MX" : 139,"FM" : 140,"MD" : 141,"MC" : 142,"MN" : 143,"MS" : 144,"ME" : 145,"MA" : 146,"MZ" : 147,"MM" : 148,"NA" : 149,"NR" : 150,"NP" : 151,"NL" : 152,"AN" : 153,"NC" : 154,"NZ" : 155,"NI" : 156,"NE" : 157,"NG" : 158,"NU" : 159,"NF" : 160,"MP" : 161,"NO" : 162,"OM" : 163,"PK" : 164,"PW" : 165,"PS" : 166,"PA" : 167,"PG" : 168,"PY" : 169,"PE" : 170,"PH" : 171,"PN" : 172,"PL" : 173,"PT" : 174,"PR" : 175,"QA" : 176,"RE" : 177,"RO" : 178,"RU" : 179,"RW" : 180,"SH" : 181,"KN" : 182,"LC" : 183,"PM" : 184,"VC" : 185,"WS" : 186,"SM" : 187,"ST" : 188,"SA" : 189,"SN" : 190,"RS" : 191,"SC" : 192,"SL" : 193,"SG" : 194,"SK" : 195,"SI" : 196,"SB" : 197,"SO" : 198,"ZA" : 199,"GS" : 200,"ES" : 201,"LK" : 202,"SD" : 203,"SR" : 204,"SJ" : 205,"SZ" : 206,"SE" : 207,"CH" : 208,"SY" : 209,"TW" : 210,"TJ" : 211,"TZ" : 212,"TH" : 213,"TG" : 214,"TK" : 215,"TO" : 216,"TT" : 217,"TN" : 218,"TR" : 219,"TM" : 220,"TC" : 221,"TV" : 222,"UG" : 223,"UA" : 224,"AE" : 225,"GB" : 226,"US" : 227,"UM" : 228,"UY" : 229,"UZ" : 230,"VU" : 231,"VE" : 232,"VN" : 233,"VG" : 234,"VI" : 235,"WF" : 236,"EH" : 237,"YE" : 238,"ZM" : 239,"ZW" : 240, }
+
   const bigNumberToInt = (bn) => {
     return parseInt(bn.toString())
   }
@@ -52,12 +54,12 @@
       var bnb = await EthBnB.deployed() 
 
       // Create a listing
-      var _location = "London"
+      var _location = 'London'
+      var _country = COUNTRIES['GB']
       var _price = 5000
-      var _shortName = "Sami's awesome place"
-      var _description = "The place is so awesome - it's just awesome"
+
         try {
-          var res = await bnb.createListing(_location, _price, _shortName, _description, {from : UNUSED_ACCOUNT})    
+          var res = await bnb.createListing(_country, _location, _price, {from : UNUSED_ACCOUNT})    
           assert(false, "Should have thrown an exception")
         }
         catch(error) {
@@ -76,11 +78,10 @@
       var res = await bnb.createAccount(_shortName, {from: accounts[0]}) 
 
       // Create a listing
-      var _location = "London"
+      var _location = 'London'
       var _price = 5000
-      var _shortName = "Sami's awesome place"
-      var _description = "The place is so awesome - it's just awesome"
-      res = await bnb.createListing(_location, _price, _shortName, _description, {from: accounts[0]})
+      var _country = COUNTRIES['GB']
+      res = await bnb.createListing(_country, _location, _price, {from: accounts[0]})
       
       // Check that an event was emitted 
       truffleAssert.eventEmitted(res, 'CreateListingEvent', (ev) => {
