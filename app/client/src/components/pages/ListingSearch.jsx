cf1cd8a67ed4ab6addd9c6d113baac68e8926abc
Sami Farhat <samikfarhat@gmail.com>
2019-02-04 18:47:35 +0100

    Listings: add dropdown

--- app/client/src/components/pages/ListingSearch.jsx
+++ app/client/src/components/pages/ListingSearch.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Listing from './Listing'
 import Img from 'react-image'
 import { Link } from 'react-router-dom'
-import RGL, { WidthProvider } from "react-grid-layout";
-import log from '../../logger'
+import RGL, { WidthProvider } from 'react-grid-layout'
+import { Dropdown } from 'semantic-ui-react'
+import Listing from './Listing'
+import log from '../../logger' // eslint-disable-line no-unused-vars
 import '../../../node_modules/react-grid-layout/css/styles.css'
 import '../../../node_modules/react-resizable/css/styles.css'
+import { countryOptions } from './common'
 
 
-const ReactGridLayout = WidthProvider(RGL);
+// countryOptions = [ { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' }, ...  ]
+
+
+const ReactGridLayout = WidthProvider(RGL)
 const mapStateToProps = state => ({ listings: state.server.listings })
 
 class ListingSearch extends Component {
@@ -23,22 +28,21 @@ class ListingSearch extends Component {
     Object.keys(self.props.listings).forEach((key) => {
       const l = self.props.listings[key]
       doms.push((
-        <div key={i} className="listing-mini">
-          <Img width="100px" src="http://localhost:3000/house-fallback.png" />
-          <Link to={"/listing/" + key}> <h5> {l.title} </h5>  </Link> 
-          <div> <em> Location: </em> <span className="location"> {l.location} </span> </div>
+        <div key={i} className='listing-mini'>
+          <Img width='100px' src='http://localhost:3000/house-fallback.png' />
+          <Link to={'/listing/' + key}> 
+          <h5> {l.title} </h5>  
+          </Link> 
+          <div> <em> Location: </em> <span className='location'> {l.location} </span> </div>
           <div> <em> Country: </em> <span className="country"> {l.country} </span> </div>
           <div> <em> Price: </em> <span className="price"> {l.price} </span> </div> 
           {/* <IPFSImage hash={l.mainImageHash} ext={l.mainImageExtension} /> */}
         </div>
       ))
-      // console.log('pushing ', i)
-      const x = i % COLS
-      const y = parseInt(i / COLS)
       layout.push({
         i: i.toString(),
         x: i % COLS,
-        y: i / COLS,
+        y: parseInt(i / COLS),
         w: 1,
         h: 1,
       })
@@ -46,9 +50,12 @@ class ListingSearch extends Component {
     })
 
     return (
+      <div> 
+        <Dropdown placeholder='Select Country' fluid search selection options={countryOptions} />
         <ReactGridLayout items={3} layout={layout} cols={3} rowHeight={30} width={300}>
           {doms}
         </ReactGridLayout>
+      </div> 
     )
   }
 }
--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import 'semantic-ui-css/semantic.min.css';
 import store from './store/index'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
