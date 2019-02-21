367d0f3b202a3dbb4db269a208cbc29fd2fc63b4
Sami Farhat <samikfarhat@gmail.com>
2019-02-21 08:36:38 +0200

    source.sh: add aliases

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 #
-# 
+#
 
 SCRIPTS_DIR=$(cd $(dirname "$0}" ) && pwd )
 ROOT_DIR=$(cd $SCRIPTS_DIR && cd .. && pwd)
@@ -15,16 +15,18 @@ lint() {
   open $d/eslint-output.html
 }
 
-if [ ! -z "$BNB_SOURCED" ]; then 
+if [ ! -z "$BNB_SOURCED" ]; then
   echo "ethbnb already sourced"
 else
   export BNB_SOURCED=1
   export PS1="$PS1🏡 " # Helps indicate if we've already sourced bnb
   alias ll="lint"
+  alias data="node $SERVER_DIR/src/data_manage.js"
+  alias setup='$SCRIPTS_DIR/setup.sh'
   alias gothere="cd $ROOT_DIR"
   alias s0="$SCRIPTS_DIR/setup.sh && node $SERVER_DIR/src/data_manage.js --chain_init=true --metadata_add=true"
   alias s9="ganache-cli"
   alias s99="pkill -f ganache-cli"
-  alias s1="npm start --prefix $CLIENT_DIR" # Start client 
-  alias s2="npm start --prefix $SERVER_DIR" # Start server 
+  alias s1="npm start --prefix $CLIENT_DIR" # Start client
+  alias s2="npm start --prefix $SERVER_DIR" # Start server
 fi
