59b770073ef79bc7af36d787a85d288c9216c5d1
Sami Farhat <samikfarhat@gmail.com>
2019-02-27 12:16:52 +0000

    sequence-diagrams: update

--- app/client/src/components/pages/ListingSearchArea.jsx
+++ app/client/src/components/pages/ListingSearchArea.jsx
@@ -2,41 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Button, Dropdown } from 'semantic-ui-react'
-import SemanticDatepicker from 'react-semantic-ui-datepickers'
 import { countryOptions } from './common'
+import EthDatePicker from './EthDatePicker'
 import { fetchListings, setSearchOptions } from '../../redux/actions'
-import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css'
 import '../../css/listing-search.css'
-import '../../css/eth-datepicker.css'
 
 class ListingSearchArea extends Component {
   constructor() {
     super()
     this.searchButtonClicked = this.searchButtonClicked.bind(this)
     this.onCountryChange = this.onCountryChange.bind(this)
-    this.onDateChange = this.onDateChange.bind(this)
     this.countryCodes = countryOptions.map(countryO => ({
       ...countryO,
       value: countryO.code,
     }))
   }
 
-  onDateChange(data) {
-    const { dispatch, countryCode } = this.props
-    if (data.length === 2) {
-      dispatch(setSearchOptions({
-        countryCode,
-        fromDate: data[0],
-        toDate: data[1],
-      }))
-    }
-  }
-
   onCountryChange(ev, data) {
-    const { dispatch, fromDate, toDate } = this.props
+    const { dispatch } = this.props
     dispatch(setSearchOptions({
-      fromDate,
-      toDate,
       countryCode: data.value,
     }))
   }
@@ -47,16 +31,10 @@ class ListingSearchArea extends Component {
   }
 
   render() {
-    const { countryCode, fromDate, toDate } = this.props
+    const { countryCode } = this.props
     return (
       <div className="listing-search-area">
-        <div className="datepicker-wrapper">
-          <SemanticDatepicker
-            onDateChange={this.onDateChange}
-            type="range"
-            selected={(!fromDate || !toDate) ? [] : [fromDate, toDate]}
-          />
-        </div>
+        <EthDatePicker />
         <div className="countrypicker-wrapper">
           <Dropdown
             className="dropdown-wrapper"
@@ -83,21 +61,15 @@ class ListingSearchArea extends Component {
 }
 
 const mapStateToProps = state => ({
-  fromDate: state.searchOptions.fromDate,
-  toDate: state.searchOptions.toDate,
   countryCode: state.searchOptions.countryCode,
 })
 
 ListingSearchArea.defaultProps = {
-  fromDate: null,
-  toDate: null,
   countryCode: null,
 }
 
 ListingSearchArea.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  fromDate: PropTypes.instanceOf(Date),
-  toDate: PropTypes.instanceOf(Date),
   countryCode: PropTypes.number,
 }
 
