d0838971a894ccdeb5ec5bc1461b051942f9bc47
Sami Farhat <samikfarhat@gmail.com>
2019-05-01 09:53:47 +0100

    backend: model/Listing.js: add totalScore and nRatings properties

--- app/server/src/models/Listing.js
+++ app/server/src/models/Listing.js
@@ -13,6 +13,11 @@ var Listing = mongoose.model('listings', new Schema({
   location: String,
   bookings: [{type: mongoose.Schema.Types.ObjectId, ref: 'bookings'}],
   images: [{type: mongoose.Schema.Types.ObjectId, ref: 'ipfs_images'}],
+  // Counts the total score received for this account
+  // Note: this is not the average
+  totalScore: Number,
+  // Counts the number of ratings received for this account
+  nRatings: Number,
 }))
 
 module.exports = Listing
