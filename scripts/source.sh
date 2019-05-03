3f99d6b334ff4c0504a4b5ec1a70748219fbbd5f
Sami Farhat <samikfarhat@gmail.com>
2019-05-03 11:03:35 +0200

    scripts/source.sh: Adjust script based on last commit

--- scripts/source.sh
+++ scripts/source.sh
@@ -7,7 +7,7 @@ ROOT_DIR=$(cd $SCRIPTS_DIR && cd .. && pwd)
 CLIENT_DIR="$ROOT_DIR/app/client"
 SERVER_DIR="$ROOT_DIR/app/server"
 
-lint() {
+lintClient() {
   d=$(mktemp -d)
   pushd $CLIENT_DIR
   eslint --config .eslintrc.json --ext .jsx --format html -o $d/eslint-output.html .
@@ -16,19 +16,16 @@ lint() {
 }
 
 if [ ! -z "$BNB_SOURCED" ]; then
-  echo "ethbnb already sourced"
+  echo "EthBnB already sourced"
 else
   export BNB_SOURCED=1
   export PS1="$PS1🏡 " # Helps indicate if we've already sourced bnb
-  alias ll="lint"
-  alias data="node $SERVER_DIR/src/data_manage.js"
-  alias setup='$SCRIPTS_DIR/setup.sh'
   alias gothere="cd $ROOT_DIR"
-  alias full_setup="$SCRIPTS_DIR/setup.sh && node $SERVER_DIR/src/data_manage.js --chain_init=true --metadata_add=true"
-  alias quick_setup="$SCRIPTS_DIR/setup.sh && node $SERVER_DIR/src/data_manage.js --chain_init=true --metadata_add=false"
-  alias s0=quick_setup
-  alias s9="ganache-cli -e 10000 -m \"eyebrow pluck bonus glove drastic nature chef rent youth dizzy magnet virtual\""
-  alias s99="pkill -f ganache-cli"
+  alias full_setup="node $SERVER_DIR/src/server.js --initTestData=true"
+  alias quick_setup="node $SERVER_DIR/src/server.js --initTestData=false"
   alias s1="npm start --prefix $CLIENT_DIR" # Start client
   alias s2="npm start --prefix $SERVER_DIR" # Start server
+  alias s9="ganache-cli -e 10000 -m \"eyebrow pluck bonus glove drastic nature chef rent youth dizzy magnet virtual\""
+  alias s0=full_setup
+  alias q0=quick_setup
 fi
