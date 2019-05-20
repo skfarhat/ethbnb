6cb134d292cd16f6f05a5df59a309ddc2002518f
Sami Farhat <samikfarhat@gmail.com>
2019-05-20 16:34:36 +0100

    client: AccountDropdown: code cleanup

--- app/client/src/components/AccountDropdown.jsx
+++ app/client/src/components/AccountDropdown.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Dropdown } from 'semantic-ui-react'
@@ -6,27 +6,36 @@ import { isSet } from '../constants/global'
 import { setAccount } from '../redux/accountActions'
 
 
-class AccountDropdown extends Component {
-  render() {
-    const { accounts, account, dispatch } = this.props
-    const options = (isSet(accounts))
-      ? accounts.map((addr, idx) => ({ key: addr, value: idx, text: addr.substr(0, 5) }))
-      : []
-    const selectedIndex = isSet(account) && isSet(accounts) ? accounts.findIndex(v => v === account.addr) : -1
-    return (
-      <Dropdown
-        noResultsMessage="No accounts."
-        value={selectedIndex}
-        options={options}
-        loading={options.length === 0}
-        onChange={(data, { value }) => {
-          const { key: addr } = options[value]
-          dispatch(setAccount(addr))
-        }
-      }
-      />
-    )
-  }
+/**
+ * React component wrapping around a Dropdown component
+ * which allows the selection of an account from the provided list.
+ *
+ * The component reads the following from state:
+ *  @accounts   to get the list of eth accounts available
+ *  @account    to figure out which account should be marked
+ *              as selected by default
+ *
+ */
+const AccountDropdown = (props) => {
+  const { accounts, account, dispatch } = props
+  const options = (isSet(accounts))
+    ? accounts.map((addr, idx) => ({ key: addr, value: idx, text: addr.substr(0, 5) }))
+    : []
+  const selectedIndex = isSet(account) && isSet(accounts)
+    ? accounts.findIndex(v => v === account.addr)
+    : -1
+  return (
+    <Dropdown
+      noResultsMessage="No accounts."
+      value={selectedIndex}
+      options={options}
+      loading={options.length === 0}
+      onChange={(data, { value }) => {
+        const { key: addr } = options[value]
+        dispatch(setAccount(addr))
+      }}
+    />
+  )
 }
 
 AccountDropdown.defaultProps = {
