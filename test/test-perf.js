59209861bef1b5f566adec430740da61cc41327d
Sami Farhat <samikfarhat@gmail.com>
2019-10-04 15:08:28 +0200

    Commit performance test

--- test/test-perf.js
+++ test/test-perf.js
@@ -3,15 +3,9 @@ const EthBnB_Basic = artifacts.require('EthBnB_Basic')
 const EthBnB = artifacts.require('EthBnB')
 
 const {
-  feb2019,
   fromFinney,
-  createListingDefault,
-  bookListingDefault,
-  bigNumberToInt,
-  BOOKING_CAPACITY,
   DEFAULT_LISTING_PRICE,
   DEFAULT_LISTING_PRICE_WEI,
-  COUNTRIES
 } = require('./utils')
 
 const getBnB = async (contractName) => {
@@ -29,67 +23,102 @@ let used = {} // Store used dates to avoid conflicts
 
 const getGas = res => res.receipt.gasUsed
 
-const getDate = (n) => {
-  const RANGE = 1000000
-  let r = Math.floor(Math.random() * RANGE)
+const getDateShifting = (i, N) => {
+  const DAY_MSEC = 86400 * 1000
+  const RANGE_DAYS = 1000000 // range of days from which we pick from
+  const rnd = Math.random()
+  let r = Math.floor((rnd + i / N) * RANGE_DAYS) * DAY_MSEC
   while (r in used) {
-    r = Math.floor(Math.random() * RANGE)
+    r = Math.floor((r + i / N) * RANGE_DAYS) * DAY_MSEC
   }
-  const timeMsec = new Date(86400 * 1000 * r).getTime() + 86400 * 1000 * n
-  return new Date(timeMsec).getTime() / 1000
+  // console.log(`${i}-${r}`)
+  return new Date(r).getTime() / 1000
 }
 
-
-const runSet = async (bnb, n, lid, a1) => {
+const runSetBoth = async (bnb, bnb2, n, lid, a1) => {
   let res
   const bids = []
+  const bids2 = []
   const NB_OF_DAYS = 1
   const d1 = { from: a1, value: fromFinney(DEFAULT_LISTING_PRICE * 2 * NB_OF_DAYS) }
 
+  console.log('Name, Optim, Basic, Day')
   // Book all of them
   for (let i = 0; i < n; i++) {
-    res = await bnb.bookListing(lid, getDate(i), NB_OF_DAYS, d1)
+    const date = getDateShifting(i, n)
+    const day = date / (86400)
+    // const date2 = new Date(day * 1000)
+    // console.log(`${date}, ${date2}`)
+    res = await bnb.bookListing(lid, date, NB_OF_DAYS, d1)
     truffleAssert.eventEmitted(res, 'BookingComplete', ev => bids.push(ev.bid))
-    console.log(`bookListing-${i}, ${getGas(res)}`)
+    const gas1 = getGas(res)
+
+    res = await bnb2.bookListing(lid, date, NB_OF_DAYS, d1)
+    truffleAssert.eventEmitted(res, 'BookingComplete', ev => bids2.push(ev.bid))
+    const gas2 = getGas(res)
+
+    console.log(`bookListing-${i}, ${gas1}, ${gas2}, ${day}`)
   }
 
   // Cancel all of them
   for (let i = 0; i < n; i++) {
-    //  Cancel all bookings
     res = await bnb.cancelBooking(lid, bids[i], { from: a1 })
     truffleAssert.eventEmitted(res, 'BookingCancelled')
-    console.log(`cancelBooking-${i}, ${getGas(res)}`)
+    const gas1 = getGas(res)
+    res = await bnb2.cancelBooking(lid, bids2[i], { from: a1 })
+    truffleAssert.eventEmitted(res, 'BookingCancelled')
+    const gas2 = getGas(res)
+
+    console.log(`cancelBooking-${i}, ${gas1}, ${gas2}, `)
   }
+
   // Book all of them
   for (let i = 0; i < n; i++) {
-    res = await bnb.bookListing(lid, getDate(i), NB_OF_DAYS, d1)
+    const date = getDateShifting(i, n)
+    const day = date / (86400)
+    res = await bnb.bookListing(lid, date, NB_OF_DAYS, d1)
     truffleAssert.eventEmitted(res, 'BookingComplete', ev => bids.push(ev.bid))
-    console.log(`bookListing-${i}, ${getGas(res)}`)
+    const gas1 = getGas(res)
+
+    res = await bnb2.bookListing(lid, date, NB_OF_DAYS, d1)
+    truffleAssert.eventEmitted(res, 'BookingComplete', ev => bids2.push(ev.bid))
+    const gas2 = getGas(res)
+
+    console.log(`bookListing-${i}, ${gas1}, ${gas2}, ${day}`)
   }
 }
 
-const testContract = (contractName) => {
-  contract(contractName, async (accounts) => {
+// for(var i = 0; i < 1000; i++) {
+//   const d = getDateShifting(i, 1000)
+//   const date = new Date(d * 1000)
+//   console.log(`${d}, ${date}`)
+// }
+
+contract('EthBnB', async (accounts) => {
+  contract('EthBnB_Basic', async (accounts2) => {
     it('First test', async () => {
       let res
       let lid
-      const bnb = await getBnB(contractName)
+      const bnb = await getBnB('EthBnB')
+      const bnb_basic = await getBnB('EthBnB_Basic')
       const [a0, a1] = accounts
       used = {} // reset it
-      const d = { from: a0, value: fromFinney(DEFAULT_LISTING_PRICE * 10000) }
+      const d = { from: a0, value: fromFinney(DEFAULT_LISTING_PRICE * 100000) }
 
       res = await bnb.createAccount('Host', { from: a0 })
       res = await bnb.createAccount('Guest', { from: a1 })
+
+      res = await bnb_basic.createAccount('Host', { from: a0 })
+      res = await bnb_basic.createAccount('Guest', { from: a1 })
+
       res = await bnb.createListing(COUNTRIES.GB, 'London', DEFAULT_LISTING_PRICE_WEI, d)
       truffleAssert.eventEmitted(res, 'CreateListingEvent', ev => lid = ev.lid)
-      console.log(`createListing: ${getGas(res)}`)
+
+      res = await bnb_basic.createListing(COUNTRIES.GB, 'London', DEFAULT_LISTING_PRICE_WEI, d)
+      truffleAssert.eventEmitted(res, 'CreateListingEvent', ev => lid = ev.lid)
 
       const N = 100
-      await runSet(bnb, N, lid,  a1)
+      await runSetBoth(bnb, bnb_basic, N, lid, a1)
     })
   })
-}
-
-
-testContract('EthBnB')
-testContract('EthBnB_Basic')
+})
