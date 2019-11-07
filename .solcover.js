b372fe9f41f34909319943dfe68ca9947fe70040
Sami Farhat <samikfarhat@gmail.com>
2019-11-07 11:26:21 +0000

    Add .solcover.js file for coverage reports

---
+++ .solcover.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testCommand: './scripts/truffle-test.sh',
+  skipFiles: [
+    'contracts/EthBnB1.sol',
+    'contracts/OptimBookerLib1.sol',
+    'OptimBookerLib1.sol',
+    'TestOptimBookerLib.sol',
+    'contracts/basic/EthBnB_Basic.sol'
+  ]
+}
