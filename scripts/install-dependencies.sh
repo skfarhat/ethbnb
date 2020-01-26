da97f1fd4925abf7cca0110bfd23a1fc15790c39
Sami Farhat <samikfarhat@gmail.com>
2020-01-26 09:58:55 +0100

    Update install-dependencies.sh script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,8 +5,16 @@ ROOT_DIR=$(cd $SCRIPTS_DIR && cd .. && pwd)
 
 pushd $ROOT_DIR > /dev/null
 
+echo "Installing global dependencies: truffle and ganache-cli"
+npm install -g truffle ganache-cli
+
+echo "Installing smart-contract dependencies"
 npm install --prefix truffle
+
+echo "Installing backend dependencies"
 npm install --prefix backend
+
+echo "Installing frontend dependencies"
 npm install --prefix frontend
 
 popd
